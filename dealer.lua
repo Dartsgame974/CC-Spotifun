6f8347043704acdae28d5978c0fa239055dedf3c
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-07-20 05:33:41 +0200

    Update dealer.lua

--- dealer.lua
+++ dealer.lua
@@ -44,95 +44,186 @@ if response then
     local secretCode = ""
     local easterEggMode = false
 
-    while true do
-      term.clear()
-      term.setCursorPos(1, 1)
+    local function displayMusicMenu()
+      local itemsPerPage = 6
+      local currentPage = 1
+      local totalOptions = #musicList
+      local totalPages = math.ceil(totalOptions / itemsPerPage)
+      local selectedIndex = 1
 
-      -- Le reste du code du menu principal ici...
+      -- Boot Menu
+      local monitor = peripheral.find("monitor")
+      monitor.setTextScale(1)
+      monitor.clear()
 
-      local _, key = os.pullEvent("key")
-      local keyName = keys.getName(key)
+      local screenWidth, screenHeight = monitor.getSize()
+      local logoHeight = 5
+      local logoText = "Spotifo"
+      local byText = "by Dartsgame"
+      local logoY = math.floor((screenHeight - logoHeight) / 2)
+      local logoX = math.floor((screenWidth - #logoText) / 2)
 
-      if keyName == "b" or keyName == "a" or keyName == "t" or keyName == "m" or keyName == "n" then
-        secretCode = secretCode .. keyName
+      while true do
+        monitor.clear()
+        monitor.setCursorPos(1, 3)
 
-        if secretCode == "batman" then
-          easterEggMode = true
-          term.setBackgroundColor(colors.purple) -- Changer la couleur de fond en violet
-          -- Charger la playlist alternative depuis le fichier "playlistdark.json"
-          local playlistDarkURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlistdark.json"
-          local responseDark = http.get(playlistDarkURL)
-          if responseDark then
-            local playlistDarkData = responseDark.readAll()
-            responseDark.close()
-            local successDark, playlistDark = pcall(textutils.unserializeJSON, playlistDarkData)
-            if successDark and type(playlistDark) == "table" then
-              musicList = {}
-              for _, entry in ipairs(playlistDark) do
-                table.insert(musicList, entry.title)
+        monitor.setTextColor(colors.green)
+        monitor.setCursorPos(1, 2)
+        monitor.write(string.rep(string.char(143), screenWidth))
+        monitor.setCursorPos(1, 3)
+        monitor.write(string.rep(" ", screenWidth))
+        monitor.setCursorPos((screenWidth - #logoText) / 2 + 1, 3)
+        monitor.write(logoText)
+        monitor.setCursorPos(1, 4)
+        monitor.write(string.rep(string.char(143), screenWidth))
+
+        local startIndex = (currentPage - 1) * itemsPerPage + 1
+        local endIndex = math.min(startIndex + itemsPerPage - 1, totalOptions)
+
+        for i = startIndex, endIndex do
+          local optionIndex = i - startIndex + 1
+          local option = musicList[i]
+
+          if optionIndex == selectedIndex then
+            monitor.setTextColor(colors.green)
+            option = option .. " "
+          else
+            monitor.setTextColor(colors.gray)
+          end
+
+          monitor.setCursorPos(1, i - startIndex + 5)
+          monitor.write(optionIndex .. " [" .. option .. "]")
+        end
+
+        monitor.setTextColor(colors.white)
+        local pageText = currentPage .. "/" .. totalPages
+        local totalText = "Titres " .. totalOptions
+        local headerText = logoText .. "  " .. pageText .. "  " .. totalText
+        local headerTextPos = (screenWidth - #headerText) / 2 + 1
+        monitor.setCursorPos(headerTextPos, 3)
+        monitor.write(headerText)
+
+        -- Options "Précédent" et "Suivant"
+        monitor.setTextColor(colors.blue)
+        monitor.setCursorPos(1, screenHeight)
+        monitor.write("Précédent")
+        monitor.setCursorPos(screenWidth - 7, screenHeight)
+        monitor.write("Suivant")
+
+        -- Vérifier la combinaison de touches pour activer le mode easter egg
+        local event, key = os.pullEvent("key")
+        local keyName = keys.getName(key)
+
+        if keyName == "b" or keyName == "a" or keyName == "t" or keyName == "m" or keyName == "n" then
+          secretCode = secretCode .. keyName
+
+          if secretCode == "batman" then
+            easterEggMode = true
+            -- Charger la playlist alternative depuis le fichier "playlistdark.json"
+            local playlistDarkURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlistdark.json"
+            local responseDark = http.get(playlistDarkURL)
+            if responseDark then
+              local playlistDarkData = responseDark.readAll()
+              responseDark.close()
+              local successDark, playlistDark = pcall(textutils.unserializeJSON, playlistDarkData)
+              if successDark and type(playlistDark) == "table" then
+                musicList = {}
+                for _, entry in ipairs(playlistDark) do
+                  table.insert(musicList, entry.title)
+                end
               end
             end
-          end
-          
-          os.sleep(0.5) -- Attendre un court délai pour réinitialiser le code secret
-          secretCode = ""
-        end
-      else
-        secretCode = ""
-      end
-        if easterEggMode then
-          -- Utiliser la playlist alternative pour le mode easter egg
-          if key == keys.up then
-            selectedIndex = selectedIndex - 1
-            if selectedIndex < 1 then
-              selectedIndex = endIndex - startIndex + 1
-            end
-          elseif key == keys.down then
-            selectedIndex = selectedIndex + 1
-            if selectedIndex > endIndex - startIndex + 1 then
-              selectedIndex = 1
-            end
-          elseif key == keys.left and currentPage > 1 then
-            currentPage = currentPage - 1
-            selectedIndex = math.min(selectedIndex, endIndex - startIndex + 1)
-          elseif key == keys.right and currentPage < totalPages then
-            currentPage = currentPage + 1
-            selectedIndex = math.min(selectedIndex, endIndex - startIndex + 1)
-          elseif key == keys.enter then
-            local selectedOption = startIndex + selectedIndex - 1
-            local selectedMusic = playlist[selectedOption]
-            playMusic(selectedMusic.title, selectedMusic.link)
+
+            os.sleep(0.5) -- Attendre un court délai pour réinitialiser le code secret
+            secretCode = ""
           end
         else
-          -- Utiliser la playlist normale pour le mode normal
-          if key == keys.up then
-            selectedIndex = selectedIndex - 1
-            if selectedIndex < 1 then
-              selectedIndex = totalOptions
-            end
-          elseif key == keys.down then
-            selectedIndex = selectedIndex + 1
-            if selectedIndex > totalOptions then
-              selectedIndex = 1
-            end
-          elseif key == keys.left then
-            currentPage = currentPage - 1
-            if currentPage < 1 then
-              currentPage = totalPages
-            end
-            selectedIndex = math.min(selectedIndex, endIndex - startIndex + 1)
-          elseif key == keys.right then
-            currentPage = currentPage + 1
-            if currentPage > totalPages then
-              currentPage = 1
-            end
-            selectedIndex = math.min(selectedIndex, endIndex - startIndex + 1)
-          elseif key == keys.enter then
-            local selectedOption = startIndex + selectedIndex - 1
-            local selectedMusic = playlist[selectedOption]
-            playMusic(selectedMusic.title, selectedMusic.link)
+          secretCode = ""
+        end
+
+while true do
+  monitor.clear()
+  monitor.setCursorPos(1, 3)
+
+  monitor.setTextColor(colors.green)
+  monitor.setCursorPos(1, 2)
+  monitor.write(string.rep(string.char(143), screenWidth))
+  monitor.setCursorPos(1, 3)
+  monitor.write(string.rep(" ", screenWidth))
+  monitor.setCursorPos((screenWidth - #logoText) / 2 + 1, 3)
+  monitor.write(logoText)
+  monitor.setCursorPos(1, 4)
+  monitor.write(string.rep(string.char(143), screenWidth))
+
+  local startIndex = (currentPage - 1) * itemsPerPage + 1
+  local endIndex = math.min(startIndex + itemsPerPage - 1, totalOptions)
+
+  for i = startIndex, endIndex do
+    local optionIndex = i - startIndex + 1
+    local option = musicList[i]
+
+    if optionIndex == selectedIndex then
+      monitor.setTextColor(colors.green)
+      option = option .. " "
+    else
+      monitor.setTextColor(colors.gray)
+    end
+
+    monitor.setCursorPos(1, i - startIndex + 5)
+    monitor.write(optionIndex .. " [" .. option .. "]")
+  end
+
+  monitor.setTextColor(colors.white)
+  local pageText = currentPage .. "/" .. totalPages
+  local totalText = "Titres " .. totalOptions
+  local headerText = logoText .. "  " .. pageText .. "  " .. totalText
+  local headerTextPos = (screenWidth - #headerText) / 2 + 1
+  monitor.setCursorPos(headerTextPos, 3)
+  monitor.write(headerText)
+
+  -- Options "Précédent" et "Suivant"
+  monitor.setTextColor(colors.blue)
+  monitor.setCursorPos(1, screenHeight)
+  monitor.write("Précédent")
+  monitor.setCursorPos(screenWidth - 7, screenHeight)
+  monitor.write("Suivant")
+
+  -- Vérifier la combinaison de touches pour activer le mode easter egg
+  local event, key = os.pullEvent("key")
+  local keyName = keys.getName(key)
+
+  if keyName == "b" or keyName == "a" or keyName == "t" or keyName == "m" or keyName == "n" then
+    secretCode = secretCode .. keyName
+
+    if secretCode == "batman" then
+      easterEggMode = true
+      -- Charger la playlist alternative depuis le fichier "playlistdark.json"
+      local playlistDarkURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlistdark.json"
+      local responseDark = http.get(playlistDarkURL)
+      if responseDark then
+        local playlistDarkData = responseDark.readAll()
+        responseDark.close()
+        local successDark, playlistDark = pcall(textutils.unserializeJSON, playlistDarkData)
+        if successDark and type(playlistDark) == "table" then
+          musicList = {}
+          for _, entry in ipairs(playlistDark) do
+            table.insert(musicList, entry.title)
           end
         end
+      end
+
+      os.sleep(0.5) -- Attendre un court délai pour réinitialiser le code secret
+      secretCode = ""
+    end
+  else
+    secretCode = ""
+  end
+
+  -- Le reste du code pour le menu principal ici...
+  -- Vous pouvez ajouter des fonctionnalités spécifiques pour le mode normal
+  -- N'oubliez pas d'ajouter les actions spécifiques au mode easter egg
+end
+
       end
     end
 
