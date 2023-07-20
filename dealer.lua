80123dead93f35431b51e8eeae48abd6da77fbe1
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-07-20 05:53:44 +0200

    Update dealer.lua

--- dealer.lua
+++ dealer.lua
@@ -21,9 +21,6 @@ if not fileExists(upgradePath) then
   shell.run("pastebin", "get", "PvwtVW1S", upgradePath)
 end
 
-local aukit = require("aukit")
-local austream = shell.resolveProgram("austream")
-
 local playlistURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlist.json"
 local response = http.get(playlistURL)
 if response then
@@ -41,8 +38,9 @@ if response then
       shell.run(austreamPath, musicURL)
     end
 
+    -- Variables pour l'easter egg
     local secretCode = ""
-    local easterEggMode = false
+    local easterEggActivated = false
 
     local function displayMusicMenu()
       local itemsPerPage = 6
@@ -52,30 +50,39 @@ if response then
       local selectedIndex = 1
 
       -- Boot Menu
-      local monitor = peripheral.find("monitor")
-      monitor.setTextScale(1)
-      monitor.clear()
-
-      local screenWidth, screenHeight = monitor.getSize()
+      term.clear()
+      local screenWidth, screenHeight = term.getSize()
       local logoHeight = 5
       local logoText = "Spotifo"
       local byText = "by Dartsgame"
       local logoY = math.floor((screenHeight - logoHeight) / 2)
       local logoX = math.floor((screenWidth - #logoText) / 2)
+      term.setTextColor(colors.green)
+      term.setCursorPos(1, logoY)
+      term.write(string.rep(string.char(143), screenWidth))
+      term.setCursorPos(1, logoY + 1)
+      term.write(string.rep(" ", screenWidth))
+      term.setCursorPos(logoX, logoY + 2)
+      term.write(logoText)
+      term.setCursorPos((screenWidth - #byText) / 2 + 1, logoY + 3)
+      term.write(byText)
+      term.setCursorPos(1, logoY + 4)
+      term.write(string.rep(string.char(143), screenWidth))
+      sleep(2) -- Attente de 2 secondes
 
       while true do
-        monitor.clear()
-        monitor.setCursorPos(1, 3)
+        term.clear()
+        term.setCursorPos(1, 3)
 
-        monitor.setTextColor(colors.green)
-        monitor.setCursorPos(1, 2)
-        monitor.write(string.rep(string.char(143), screenWidth))
-        monitor.setCursorPos(1, 3)
-        monitor.write(string.rep(" ", screenWidth))
-        monitor.setCursorPos((screenWidth - #logoText) / 2 + 1, 3)
-        monitor.write(logoText)
-        monitor.setCursorPos(1, 4)
-        monitor.write(string.rep(string.char(143), screenWidth))
+        term.setTextColor(colors.green)
+        term.setCursorPos(1, 2)
+        term.write(string.rep(string.char(143), term.getSize()))
+        term.setCursorPos(1, 3)
+        term.write(string.rep(" ", term.getSize()))
+        term.setCursorPos((term.getSize() - #logoText) / 2 + 1, 3)
+        term.write(logoText)
+        term.setCursorPos(1, 4)
+        term.write(string.rep(string.char(143), term.getSize()))
 
         local startIndex = (currentPage - 1) * itemsPerPage + 1
         local endIndex = math.min(startIndex + itemsPerPage - 1, totalOptions)
@@ -85,145 +92,69 @@ if response then
           local option = musicList[i]
 
           if optionIndex == selectedIndex then
-            monitor.setTextColor(colors.green)
+            term.setTextColor(colors.green)
             option = option .. " "
           else
-            monitor.setTextColor(colors.gray)
+            term.setTextColor(colors.gray)
           end
 
-          monitor.setCursorPos(1, i - startIndex + 5)
-          monitor.write(optionIndex .. " [" .. option .. "]")
+          print(optionIndex, " [" .. option .. "]")
         end
 
-        monitor.setTextColor(colors.white)
+        term.setTextColor(colors.white)
         local pageText = currentPage .. "/" .. totalPages
         local totalText = "Titres " .. totalOptions
         local headerText = logoText .. "  " .. pageText .. "  " .. totalText
-        local headerTextPos = (screenWidth - #headerText) / 2 + 1
-        monitor.setCursorPos(headerTextPos, 3)
-        monitor.write(headerText)
+        local headerTextPos = (term.getSize() - #headerText) / 2 + 1
+        term.setCursorPos(headerTextPos, 3)
+        term.write(headerText)
 
-        -- Options "Précédent" et "Suivant"
-        monitor.setTextColor(colors.blue)
-        monitor.setCursorPos(1, screenHeight)
-        monitor.write("Précédent")
-        monitor.setCursorPos(screenWidth - 7, screenHeight)
-        monitor.write("Suivant")
+        term.setCursorPos(1, itemsPerPage + 7)
+        term.write(string.char(17))
+        term.setCursorPos(term.getSize(), itemsPerPage + 7)
+        term.write(string.char(16))
 
-        -- Vérifier la combinaison de touches pour activer le mode easter egg
-        local event, key = os.pullEvent("key")
+        local _, key = os.pullEvent("key")
         local keyName = keys.getName(key)
 
         if keyName == "b" or keyName == "a" or keyName == "t" or keyName == "m" or keyName == "n" then
+          -- Ajouter le caractère de la touche pressée à la chaîne secrète
           secretCode = secretCode .. keyName
 
+          -- Vérifier si la chaîne secrète correspond au code caché (b-a-t-m-a-n)
           if secretCode == "batman" then
-            easterEggMode = true
+            easterEggActivated = true
+            -- Changer ici la couleur de fond en violet pour l'easter egg
             -- Charger la playlist alternative depuis le fichier "playlistdark.json"
-            local playlistDarkURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlistdark.json"
-            local responseDark = http.get(playlistDarkURL)
-            if responseDark then
-              local playlistDarkData = responseDark.readAll()
-              responseDark.close()
-              local successDark, playlistDark = pcall(textutils.unserializeJSON, playlistDarkData)
-              if successDark and type(playlistDark) == "table" then
-                musicList = {}
-                for _, entry in ipairs(playlistDark) do
-                  table.insert(musicList, entry.title)
-                end
-              end
-            end
-
-            os.sleep(0.5) -- Attendre un court délai pour réinitialiser le code secret
+            playlistURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlistdark.json"
+            os.sleep(0.5)
             secretCode = ""
           end
         else
           secretCode = ""
         end
 
-while true do
-  monitor.clear()
-  monitor.setCursorPos(1, 3)
-
-  monitor.setTextColor(colors.green)
-  monitor.setCursorPos(1, 2)
-  monitor.write(string.rep(string.char(143), screenWidth))
-  monitor.setCursorPos(1, 3)
-  monitor.write(string.rep(" ", screenWidth))
-  monitor.setCursorPos((screenWidth - #logoText) / 2 + 1, 3)
-  monitor.write(logoText)
-  monitor.setCursorPos(1, 4)
-  monitor.write(string.rep(string.char(143), screenWidth))
-
-  local startIndex = (currentPage - 1) * itemsPerPage + 1
-  local endIndex = math.min(startIndex + itemsPerPage - 1, totalOptions)
-
-  for i = startIndex, endIndex do
-    local optionIndex = i - startIndex + 1
-    local option = musicList[i]
-
-    if optionIndex == selectedIndex then
-      monitor.setTextColor(colors.green)
-      option = option .. " "
-    else
-      monitor.setTextColor(colors.gray)
-    end
-
-    monitor.setCursorPos(1, i - startIndex + 5)
-    monitor.write(optionIndex .. " [" .. option .. "]")
-  end
-
-  monitor.setTextColor(colors.white)
-  local pageText = currentPage .. "/" .. totalPages
-  local totalText = "Titres " .. totalOptions
-  local headerText = logoText .. "  " .. pageText .. "  " .. totalText
-  local headerTextPos = (screenWidth - #headerText) / 2 + 1
-  monitor.setCursorPos(headerTextPos, 3)
-  monitor.write(headerText)
-
-  -- Options "Précédent" et "Suivant"
-  monitor.setTextColor(colors.blue)
-  monitor.setCursorPos(1, screenHeight)
-  monitor.write("Précédent")
-  monitor.setCursorPos(screenWidth - 7, screenHeight)
-  monitor.write("Suivant")
-
-  -- Vérifier la combinaison de touches pour activer le mode easter egg
-  local event, key = os.pullEvent("key")
-  local keyName = keys.getName(key)
-
-  if keyName == "b" or keyName == "a" or keyName == "t" or keyName == "m" or keyName == "n" then
-    secretCode = secretCode .. keyName
-
-    if secretCode == "batman" then
-      easterEggMode = true
-      -- Charger la playlist alternative depuis le fichier "playlistdark.json"
-      local playlistDarkURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlistdark.json"
-      local responseDark = http.get(playlistDarkURL)
-      if responseDark then
-        local playlistDarkData = responseDark.readAll()
-        responseDark.close()
-        local successDark, playlistDark = pcall(textutils.unserializeJSON, playlistDarkData)
-        if successDark and type(playlistDark) == "table" then
-          musicList = {}
-          for _, entry in ipairs(playlistDark) do
-            table.insert(musicList, entry.title)
+        if keyName == keys.up then
+          selectedIndex = selectedIndex - 1
+          if selectedIndex < 1 then
+            selectedIndex = endIndex - startIndex + 1
           end
+        elseif keyName == keys.down then
+          selectedIndex = selectedIndex + 1
+          if selectedIndex > endIndex - startIndex + 1 then
+            selectedIndex = 1
+          end
+        elseif keyName == keys.left and currentPage > 1 then
+          currentPage = currentPage - 1
+          selectedIndex = math.min(selectedIndex, endIndex - startIndex + 1)
+        elseif keyName == keys.right and currentPage < totalPages then
+          currentPage = currentPage + 1
+          selectedIndex = math.min(selectedIndex, endIndex - startIndex + 1)
+        elseif keyName == keys.enter then
+          local selectedOption = startIndex + selectedIndex - 1
+          local selectedMusic = playlist[selectedOption]
+          playMusic(selectedMusic.title, selectedMusic.link)
         end
-      end
-
-      os.sleep(0.5) -- Attendre un court délai pour réinitialiser le code secret
-      secretCode = ""
-    end
-  else
-    secretCode = ""
-  end
-
-  -- Le reste du code pour le menu principal ici...
-  -- Vous pouvez ajouter des fonctionnalités spécifiques pour le mode normal
-  -- N'oubliez pas d'ajouter les actions spécifiques au mode easter egg
-end
-
       end
     end
 
